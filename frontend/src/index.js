15288f7e7b71c8fed8923f0e31a3a64d9bbb9270
Andric Tham <andric@nuisance.co>
2018-01-15 07:53:15 +0800

    chore: Set up redux-thunk and install axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,13 @@ import './index.css'
 import App from './App'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import reducer from './reducers'
 
-const store = createStore(
-	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// use the compose function from Redux DevTools Chrome Extension if it’s available, if not, use the one from redux.
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
 	<Provider store={store}>
