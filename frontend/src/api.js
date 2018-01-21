eea800e8e704c1dc23844b7ec11d782b9fe1b47b
Andric Tham <andric@nuisance.co>
2018-01-21 09:38:54 +0800

    chore: Flesh out all API requests

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,9 +6,9 @@ const api = axios.create({
 	headers: { Authorization: 'Basic amFzb25oaWNrOg==' },
 })
 
-//														//
-//		C A T E G O R I E S			//
-//														//
+// ===	===	===	===	=== === === ===
+//			C A T E G O R I E S
+// ===	===	===	===	=== === === ===
 
 // GET /categories
 // Get all of the categories available for the app.
@@ -22,9 +22,9 @@ export const getCategoryPosts = category => {
 	return api.get(`/${category}/posts`).then(res => res.data)
 }
 
-//									//
-//		P O S T S			//
-//									//
+// ===	===	===	===
+//		P O S T S
+// ===	===	===	===
 
 // GET /posts
 // Get all of the posts. Useful for the main page when no category is selected.
@@ -69,10 +69,22 @@ export const votePost = (id, vote) => {
 
 // PUT /posts/:id
 // Edit the details of an existing post.
+export const editPost = (id, title, body) => {
+	return api
+		.put(`/posts/${id}`, {
+			id,
+			title,
+			body,
+		})
+		.then(res => res.data)
+}
 
 // DELETE /posts/:id
 // Sets the deleted flag for a post to 'true'.
 // Sets the parentDeleted flag for all child comments to 'true'.
+export const deletePost = id => {
+	return api.delete(`posts/${id}`).then(res => res.data)
+}
 
 // GET /posts/:id/comments
 // Get all the comments for a single post.
@@ -80,12 +92,23 @@ export const getPostComments = id => {
 	return api.get(`/posts/${id}/comments`).then(res => res.data)
 }
 
-//												//
-//		C O M M E N T S			//
-//												//
+// ===	===	===	===	===	===
+//		 C O M M E N T S
+// ===	===	===	===	===	===
 
 // POST /comments
 // Add a comment to a post.
+export const addComment = ({ id, timestamp, body, author, parentId }) => {
+	return api
+		.post(`/comments`, {
+			id,
+			timestamp,
+			body,
+			author,
+			parentId,
+		})
+		.then(res => res.data)
+}
 
 // GET /comments/:id
 // Get the details for a single comment.
@@ -95,9 +118,28 @@ export const getComment = id => {
 
 // POST /comments/:id
 // Used for voting on a comment.
+export const voteComment = (id, vote) => {
+	return api
+		.post(`/comments/${id}`, {
+			id,
+			option: vote,
+		})
+		.then(res => res.data)
+}
 
 // PUT /comments/:id
 // Edit the details of an existing comment.
+export const editComment = (id, timestamp, body) => {
+	return api
+		.post(`/comments/${id}`, {
+			timestamp,
+			body,
+		})
+		.then(res => res.data)
+}
 
 // DELETE /comments/:id
 // Sets a comment's deleted flag to true.
+export const deleteComment = id => {
+	return api.delete(`comments/${id}`).then(res => res.data)
+}
