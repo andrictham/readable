b326b9f9b6da0a9a7d44165af0d6a1a90dcfef26
Andric Tham <andric@nuisance.co>
2018-02-08 00:51:22 +0800

    feat: Vote counter UI (work-in-progress)

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import moment from 'moment'
-import { Card, Badge, Subhead } from 'rebass'
+import { Flex, Box, Card, Badge, Subhead, Divider } from 'rebass'
 import styled from 'styled-components'
-import { MAIN } from '../utils/colors'
+import { MAIN, FADED, BG_BOTTOM } from '../utils/colors'
+import { FaArrowUp, FaArrowDown } from 'react-icons/lib/fa'
 
 const Post = ({ id, title, body, author, category, timestamp, voteScore }) => (
 	<PostCard p={3} m={3}>
@@ -16,13 +17,49 @@ const Post = ({ id, title, body, author, category, timestamp, voteScore }) => (
 			</Badge>
 		</Subhead>
 
-		<p>{body}</p>
-		<p>Votes: {voteScore}</p>
+		<PostBody>{body}</PostBody>
+
+		<VoteCounter voteScore={voteScore} />
 	</PostCard>
 )
 
 const PostCard = styled(Card)`
 	box-shadow: none;
+`
+
+const PostBody = styled.p`
+	line-height: 150%;
+`
+
+const VoteCounter = ({ voteScore }) => {
+	return (
+		<Flex align="center" my={3}>
+			<Box pr={3}>
+				<UpVoteButton />
+			</Box>
+			<Box pr={3}>
+				<VoteScoreNumber>{voteScore}</VoteScoreNumber>
+			</Box>
+			<Box pr={3}>
+				<DownVoteButton />
+			</Box>
+		</Flex>
+	)
+}
+
+const VoteScoreNumber = styled.div`
+	text-align: center;
+	padding-top: 2px;
+	font-size: 1.2rem;
+	font-weight: 700;
+`
+
+const UpVoteButton = styled(FaArrowUp)`
+	color: ${FADED};
+`
+
+const DownVoteButton = styled(FaArrowDown)`
+	color: ${FADED};
 `
 
 export default Post
--- frontend/src/utils/colors.js
+++ frontend/src/utils/colors.js
@@ -3,11 +3,13 @@ const RED = '#E20000'
 const ORANGE = '#FF6F17'
 const TANGERINE = '#FE7D21'
 const YELLOW = '#FAD961'
+const CONCRETE = '#A8A8A8'
 const SMOKE = '#F6F6F6'
 const WHITE = '#FFFFFF'
 
 // Decisions
 export const MAIN = TANGERINE
+export const FADED = CONCRETE
 export const BG_TOP = WHITE
 export const BG_BOTTOM = SMOKE
 export const GRADIENT_1 = YELLOW
