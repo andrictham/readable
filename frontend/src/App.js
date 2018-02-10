2ae93cbec63b96ec3ea3f0f8e16fe87c9ad3d5e3
Andric Tham <andric@nuisance.co>
2018-02-10 19:54:52 +0800

    feat: Make <PostCard /> clickable into a detail view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar'
 import AllPosts from './components/AllPosts'
 import AddPost from './components/AddPost'
 import EditPost from './components/EditPost'
+import PostCard from './components/PostCard'
 import styled, { injectGlobal } from 'styled-components'
 import { BG_BOTTOM } from './utils/colors'
 
@@ -13,6 +14,7 @@ const App = props => (
 		<Switch>
 			<Route exact path="/add" component={AddPost} />
 			<Route exact path="/edit/:id" component={EditPost} />
+			<Route exact path="/post/:id" component={PostCard} />
 			<Route exact path="/:category?" component={AllPosts} />
 		</Switch>
 	</Main>
--- frontend/src/components/PostCard.js
+++ frontend/src/components/PostCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import moment from 'moment'
 import { Flex, Box, Card, Badge, Subhead, Text } from 'rebass'
 import styled from 'styled-components'
@@ -7,42 +8,11 @@ import { MAIN, SHADOW } from '../utils/colors'
 import { TRANSITION_SMOOTH } from '../utils/transitions'
 import VoteCounter from '../components/VoteCounter'
 
-const PostCard = ({
-	id,
-	title,
-	body,
-	author,
-	category,
-	timestamp,
-	voteScore,
-	commentCount,
-}) => (
+const PostCard = props => (
 	<PostCardContainer p={3} m={3} width={[1, 5 / 6, null, 3 / 4]}>
-		<p>
-			<strong>{author}</strong> &nbsp; {moment(timestamp).fromNow()}
-		</p>
-		<Subhead>
-			{title}
-			<Badge bg={MAIN} ml={2} px={2}>
-				{category}
-			</Badge>
-		</Subhead>
-
-		<PostBody>{body}</PostBody>
-		<Flex align="center">
-			<Box w={1 / 2}>
-				<VoteCounter voteScore={voteScore} />
-			</Box>
-			<Box w={1 / 2}>
-				<Text right>
-					<Pluralize
-						singular="comment"
-						plural="comments"
-						count={commentCount}
-					/>
-				</Text>
-			</Box>
-		</Flex>
+		<StyledLink to={`/post/${props.id}`}>
+			<PostContents {...props} />
+		</StyledLink>
 	</PostCardContainer>
 )
 
@@ -54,7 +24,54 @@ const PostCardContainer = styled(Card)`
 	}
 `
 
-const PostBody = styled.p`
+const StyledLink = styled(Link)`
+	text-decoration: none;
+	color: inherit;
+`
+
+const PostContents = ({
+	id,
+	title,
+	body,
+	author,
+	category,
+	timestamp,
+	voteScore,
+	commentCount,
+}) => {
+	return (
+		<div>
+			<p>
+				<strong>{author}</strong> &nbsp; {moment(timestamp).fromNow()}
+			</p>
+			<Subhead>
+				{title}
+				<Badge bg={MAIN} ml={2} px={2}>
+					{category}
+				</Badge>
+			</Subhead>
+
+			<PostContentsBody>{body}</PostContentsBody>
+
+			<Flex align="center">
+				<Box w={1 / 2}>
+					<VoteCounter voteScore={voteScore} />
+				</Box>
+				<Box w={1 / 2}>
+					<Text right>
+						<Pluralize
+							singular="comment"
+							plural="comments"
+							count={commentCount}
+						/>
+					</Text>
+				</Box>
+			</Flex>
+		</div>
+	)
+}
+
+const PostContentsBody = styled.p`
 	line-height: 150%;
 `
 
