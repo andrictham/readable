a5de8a7fcf94e1071f1768992fe1f088b77e6081
Andric Tham <andric@nuisance.co>
2018-02-10 20:33:47 +0800

    feat: Show post contents in post detail view

--- frontend/src/modules/Posts/PostDetail.js
+++ frontend/src/modules/Posts/PostDetail.js
@@ -7,11 +7,41 @@ import { BG_TOP } from '../../utils/colors'
 import PostContents from './components/PostContents'
 
 class PostDetail extends Component {
+	state = {
+		isLoading: true,
+	}
+
+	componentDidMount() {
+		const { getPostRequest, match } = this.props
+		getPostRequest(match.params.id)
+	}
+
+	componentWillReceiveProps(nextProps) {
+		// If component is rerendering because it’s receiving new props from Redux, we want to set loading to false
+		if (nextProps.currentPost.id) {
+			this.setState(() => ({
+				isLoading: false,
+			}))
+		}
+	}
+
+	// TODO: Loading state
+
 	render() {
+		const { currentPost } = this.props
 		return (
 			<Flex>
 				<Box w={1} p={3} bg={BG_TOP}>
-					<PostContents />
+					<PostContents
+						id={currentPost.id}
+						title={currentPost.title}
+						body={currentPost.body}
+						author={currentPost.author}
+						category={currentPost.category}
+						timestamp={currentPost.timestamp}
+						voteScore={currentPost.voteScore}
+						commentCount={currentPost.commentCount}
+					/>
 				</Box>
 			</Flex>
 		)
