6863ffc6dbe8b6d84e6f531eca06c1c45427638c
Andric Tham <andric@nuisance.co>
2018-02-10 21:30:39 +0800

    chore: Style submit button in add/edit views to match app theme

--- frontend/src/modules/AddEditPost/components/EditPostForm.js
+++ frontend/src/modules/AddEditPost/components/EditPostForm.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Field } from 'redux-form'
 import { Box, Label, Input, Textarea, Button } from 'rebass'
 import styled, { css } from 'styled-components'
+import { MAIN } from '../../../utils/colors'
+import { TRANSITION_SNAPPY } from '../../../utils/transitions'
 
 const EditPostForm = ({
 	isEditing,
@@ -11,7 +13,11 @@ const EditPostForm = ({
 	handleSubmit, // a built-in method that Redux Form gives us for free.
 	onSubmit, // our custom submit function, that we will pass to Redux Form
 }) => (
-	<Box is="form" onSubmit={handleSubmit(onSubmit)} width={[1, 3 / 4, 1 / 2]}>
+	<Box
+		is="form"
+		onSubmit={handleSubmit(onSubmit)}
+		width={[1, 3 / 4, null, 5 / 6]}
+	>
 		{/* <Label mt={2}>Category</Label>
 		 TODO: Use react-select and load in the list of categories here */}
 		<Field
@@ -38,9 +44,13 @@ const EditPostForm = ({
 			placeholder="Say something nice or wicked"
 			disabled={isLoading}
 		/>
-		<Button type="submit" disabled={pristine || submitting || isLoading} my={3}>
+		<SubmitButton
+			type="submit"
+			disabled={pristine || submitting || isLoading}
+			my={3}
+		>
 			{isEditing ? 'Update Post' : 'Add Post'}
-		</Button>
+		</SubmitButton>
 	</Box>
 )
 
@@ -96,6 +106,13 @@ const StyledTextarea = styled(Textarea)`
 		css`
 			cursor: not-allowed;
 		`};
+`
+
+const SubmitButton = styled(Button)`
+	background-color: ${MAIN};
+	cursor: pointer;
+	padding: 0.8rem 1.4rem;
+	transition: ${TRANSITION_SNAPPY};
 `
 
 export default EditPostForm
