12de518c190c72e6a399cf5646300ae15870f825
Andric Tham <andric@nuisance.co>
2018-02-11 20:22:31 +0800

    feat: Make vote counter buttons

--- frontend/src/components/VoteCounter.js
+++ frontend/src/components/VoteCounter.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Flex, Box } from 'rebass'
-import styled from 'styled-components'
-import { FADED } from '../utils/colors'
+import styled, { css } from 'styled-components'
+import { FADED, MAIN } from '../utils/colors'
+import { TRANSITION_SNAPPY } from '../utils/transitions'
 import { FaArrowUp, FaArrowDown } from 'react-icons/lib/fa'
 
 const VoteCounter = ({ voteScore }) => {
 	return (
 		<Flex align="center" my={3}>
 			<Box pr={3}>
-				<UpVoteButton />
+				<VoteButton direction="up" />
 			</Box>
 			<Box pr={3}>
 				<VoteScoreNumber>{voteScore}</VoteScoreNumber>
 			</Box>
 			<Box pr={3}>
-				<DownVoteButton />
+				<VoteButton direction="down" />
 			</Box>
 		</Flex>
 	)
@@ -27,12 +28,45 @@ const VoteScoreNumber = styled.div`
 	font-weight: 700;
 `
 
-const UpVoteButton = styled(FaArrowUp)`
-	color: ${FADED};
-`
+const VoteButton = ({ direction }) => {
+	return (
+		<ArrowButton
+			direction={direction}
+			onClick={e => {
+				e.preventDefault()
+			}}
+		>
+			{direction === 'up' && <FaArrowUp />}
+			{direction === 'down' && <FaArrowDown />}
+		</ArrowButton>
+	)
+}
 
-const DownVoteButton = styled(FaArrowDown)`
-	color: ${FADED};
+const ArrowButton = styled.button`
+	border: none;
+	font-size: inherit;
+	cursor: pointer;
+	padding: 0;
+	&:focus {
+		outline: 0;
+	}
+	& svg {
+		color: ${FADED};
+		transition: ${TRANSITION_SNAPPY};
+		&:hover {
+			color: ${MAIN};
+			${props =>
+				props.direction === 'up' &&
+				css`
+					transform: translateY(-1px);
+				`};
+			${props =>
+				props.direction === 'down' &&
+				css`
+					transform: translateY(1px);
+				`};
+		}
+	}
 `
 
 export default VoteCounter
